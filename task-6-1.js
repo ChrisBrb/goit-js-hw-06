87610ce9791f87e98010cdcc33074625f86bc203
Christina Burbulevych <christina.brb@gmail.com>
2020-09-23 22:27:30 +0300

    task-6-1

---
+++ task-6-1.js
@@ -0,0 +1,19 @@
+//map
+//Получи массив имен всех пользователей (свойство name) используя деструктурирующее присваивание для параметра функции ({name}) без пробелов и переносов на новую строку.
+
+//Используй только перебирающие методы массива которые не изменяют (не мутируют) исходный массив. Т.е. нельзя использовать for, splice, push и т.п. мутирующие методы.
+// Write code under this line
+// Write code under this line
+const getUserNames = (array) => array.map(({name})=>name);
+
+console.log(getUserNames(users));
+
+/* [
+  "Moore Hensley",
+  "Sharlene Bush",
+  "Ross Vazquez",
+  "Elma Head",
+  "Carey Barr",
+  "Blackburn Dotson",
+  "Sheree Anthony",
+] */
